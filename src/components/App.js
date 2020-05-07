53925587eef7139d23339dca8dc801095065ffed
Moshe Boord <mcboord@gmail.com>
2020-05-07 16:14:25 -0400

    Basic Working Project

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <h1>Job Board</h1>
+        </div>
+    );
+}
+
+let age = 0;
+console.log(age ?? "no age given");
+console.log(5n + 3n);
+
+export default App;
