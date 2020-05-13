37f21d0ce6f3a800b98f77173620e224f2b16d59
John Tieu <johnson.tieu@gmail.com>
2020-05-13 16:49:34 -0400

    routing is ok

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
 import React from "react";
-import Provider from "react-redux";
-import { store } from "../redux";
+import { Route, Link, Switch } from 'react-router-dom'
+import EmployeeView from './Views/EmployeeView'
+import EmployerView from './Views/EmployerView'
 
 function App() {
-    return (
-        <Provider store={store}>
-            <h1>Job Board</h1>
-        </Provider>
-    );
+  return (
+    <>
+      <Link to='/employeeView'>Employee View</Link>
+      <Link to='/employerView'>Employer View</Link>
+      <Switch>
+
+        {/* <Route path="/login" component={Login} /> */}
+        {/* <Route exact path='/' component={Home}></Route> */}
+        <Route path='/employeeView' component={EmployeeView}></Route>
+        <Route path='/employerView' component={EmployerView}></Route>
+      </Switch>
+    </>
+  );
 }
 
 export default App;
+
+
+
+
+
+
---
+++ src/components/Views/EmployeeView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const EmployeeView = () => {
+
+  return (
+
+    <h1>this is the employee view</h1>
+  )
+
+}
+
+export default EmployeeView
